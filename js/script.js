4815dc02f3aef4d9b986d8beabb77e9f96308648
Vadim <vadimlazuk33@gmail.com>
2023-04-23 17:02:29 +0300

    created admin pages

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-const burger = document.querySelector(".burger");
-const navigation = document.querySelector(".header-nav");
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.header-nav');
 
 function burgerAnimation() {
-  burger.classList.toggle("active");
-  navigation.classList.toggle("open");
+  burger.classList.toggle('active');
+  navigation.classList.toggle('open');
 }
 
-burger.addEventListener("click", burgerAnimation);
+burger.addEventListener('click', burgerAnimation);
 
-var mobile_menu = $(".balance-page__menu");
-$(".mobile-dop_menu").on("click", function (e) {
-    if(mobile_menu.hasClass("activ"))
-    {
-        mobile_menu.removeClass("activ");
-    } else {
-        mobile_menu.addClass("activ");
-    }
+var mobile_menu = $('.balance-page__menu');
+$('.mobile-dop_menu').on('click', function (e) {
+  if (mobile_menu.hasClass('activ')) {
+    mobile_menu.removeClass('activ');
+  } else {
+    mobile_menu.addClass('activ');
+  }
 });
 
-$(".balance-page__menu").on("click", function (e) {
+$('.balance-page__menu').on('click', function (e) {
   console.log(1);
-  if ($(e.target).hasClass("balance-page__menu")) {
-    mobile_menu.removeClass("activ");
+  if ($(e.target).hasClass('balance-page__menu')) {
+    mobile_menu.removeClass('activ');
   }
 });
